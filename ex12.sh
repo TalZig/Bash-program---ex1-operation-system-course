728f8bce4031a4606011f6364803ac1e1eb4683a
Tal Zigdon <50695642+TalZig@users.noreply.github.com>
2020-04-01 17:51:43 +0300

    now the files good

--- ex12.sh
+++ ex12.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #Tal Zigdon 313326019
 numOfWords=1
+#cd $1
 for fwd in "$1"/*
 do
   endOfFile="${fwd: -4}"
   #check if the end of the name file is .txt
   if [ $endOfFile == ".txt" ]
     then
-      echo "$numOfWords $fwd is a file"
+    printf "$numOfWords "
+    echo "$fwd is a file"| sed "s/.*\///"
+      #echo "$numOfWords $fwd is a file"
       numOfWords=$[$numOfWords+1]
   fi
 done
@@ -16,7 +19,8 @@ do
   #check if the end of the name file is'nt .txt
   if [[ -d "$fwd" ]]
     then
-      echo "$numOfWords $fwd is a directory"
+      printf "$numOfWords "
+      echo "$fwd is a directory"| sed "s/.*\///"
       numOfWords=$[$numOfWords+1]
   fi
 done
--- ex14.sh
+++ ex14.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #Tal Zigdon 313326019
 input=$2
+balance=0
 #cat $2 | while read line
-cat $2 | while read line || [ -n "$line" ];
-do
+#cat $2 | while read line || [ -n "$line" ];
+while read  line|| [ -n "$line" ]; do
+    name="$line"
   #check if the line start with the name, if yes print
   if [[ $line == "$1 "* ]]
   then
-  echo "$line"
+  for name in $line
+  do
+    if  [[ $name == ?(-)+([0-9]) ]] ; then
+      #balance=$(($balance+$name))
+      #balance=$[$balance+$name]
+      balance=$[balance+name] 
+    fi
+  done
+  echo $line
   fi
-done
+done < $2
+echo "Total balance: $balance"
